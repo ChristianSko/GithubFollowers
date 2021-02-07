579a736dfd1e46f0f8ff937ae090d13b1e74f8c8
Christian Skorobogatow <christiansko@icloud.com>
2021-02-07 01:11:54 +0100

    refactored VC's into functions that create VC's & also added system icons to tabbar

--- GithubFollowers/SceneDelegate.swift
+++ GithubFollowers/SceneDelegate.swift
@@ -16,17 +16,46 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        let searchNC = UINavigationController(rootViewController: SearchVC())
-        let favoritesNC = UINavigationController(rootViewController: FavoriteListVC())
+    
         
-        
-        let tabbar = UITabBarController()
-        tabbar.viewControllers = [searchNC, favoritesNC¡]
         
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
         window?.rootViewController = UITabBarController()
+        window?.rootViewController = createTabbar()
         window?.makeKeyAndVisible()
+    }
+    
+    
+    func createSearchNC() -> UINavigationController {
+        
+        let searchVC = SearchVC()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        return UINavigationController(rootViewController: searchVC)
+        
+    }
+    
+    
+    func createFavoritesNC() -> UINavigationController {
+        
+        let favoritesVC = FavoriteListVC()
+        favoritesVC.title = "Favorites"
+        favoritesVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        
+        return UINavigationController(rootViewController: favoritesVC)
+        
+        
+    }
+    
+    func createTabbar() -> UITabBarController {
+        
+        let tabbar = UITabBarController()
+        UITabBar.appearance().tintColor = .systemGreen
+        tabbar.viewControllers = [createSearchNC(), createFavoritesNC()]
+        
+        return tabbar
+        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
