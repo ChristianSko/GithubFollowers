3b3c0a599632468941c65aa9be003cd84e0474cb
Christian Skorobogatow <christiansko@icloud.com>
2021-02-15 21:36:24 +0100

    Created Network call to get Git followers

--- GithubFollowers/Screens/SearchVC.swift
+++ GithubFollowers/Screens/SearchVC.swift
@@ -46,7 +46,7 @@ class SearchVC: UIViewController {
         }
         
         let followerListVC      = FollowerListVC()
-        followerListVC.userName = userNameTextField.text
+        followerListVC.username = userNameTextField.text
         followerListVC.title    = userNameTextField.text
         navigationController?.pushViewController(followerListVC, animated: true)
         
