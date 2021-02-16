472a38a84e25d651bb2e907cb61959ddd9c65071
Christian Skorobogatow <christiansko@icloud.com>
2021-02-16 20:07:20 +0100

    refactored error messages into enums for better readability

--- GithubFollowers/Model/NetworkManager.swift
+++ GithubFollowers/Model/NetworkManager.swift
@@ -13,12 +13,12 @@ class NetworkManager {
     
     private init() {}
     
-    func getFollowers(for username: String, page: Int, completed: @escaping ([Follower]?, String?) -> Void) {
+    func getFollowers(for username: String, page: Int, completed: @escaping ([Follower]?, ErrorMessage?) -> Void) {
         let endpoint = baseURL + "\(username)/followers?per_page=100&page=\(page)"
         
         guard let url = URL(string: endpoint) else {
             
-            completed(nil, "This username created an invalid request. Please try again.")
+            completed(nil, ErrorMessage.invalidUsername)
             
             return
             
@@ -26,17 +26,17 @@ class NetworkManager {
         
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
             if let _ = error {
-                completed(nil, "Unable to complete your request. Please check your internet")
+                completed(nil, ErrorMessage.unableToComplete)
                 return
             }
             
             
             guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                completed(nil, "Invalid response from the server. Please try again.")
+                completed(nil, ErrorMessage.invalidResponse)
                 return
             }
             guard let data = data else {
-                completed(nil, "The data received from the server was invalid. Please try again")
+                completed(nil, ErrorMessage.invalidData)
                 return
             }
             do {
@@ -45,7 +45,7 @@ class NetworkManager {
                 let followers = try decoder.decode([Follower].self, from: data)
                 completed(followers, nil)
             } catch {
-                completed(nil, "The data received from the server was invalid. Please try again")
+                completed(nil, ErrorMessage.invalidData)
             }
             
         }
