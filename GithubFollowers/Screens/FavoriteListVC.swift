d370700b2538fe69cd6a845aba3343989b8be7b6
Christian Skorobogatow <christiansko@icloud.com>
2021-05-24 23:38:25 +0200

    added functionality of saving Github Follower to Userdefaults

--- GithubFollowers/Screens/FavoriteListVC.swift
+++ GithubFollowers/Screens/FavoriteListVC.swift
@@ -13,6 +13,14 @@ class FavoriteListVC: UIViewController {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBlue
+        PersistenceManager.retrieveFavorites { result in
+            switch result {
+            case .success(let favorites):
+                print(favorites)
+            case .failure(let error):
+                break
+            }
+        }
     }
     
 
