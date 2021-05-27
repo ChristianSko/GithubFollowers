e43f9720050f32149f58fe116e999b0e10a0dc43
Christian Skorobogatow <christiansko@icloud.com>
2021-05-27 22:26:10 +0200

    refactored Tabbar configuration into a TabbarController

---
+++ GithubFollowers/Custom Views/TabbarControllers/GFTabbarController.swift
@@ -0,0 +1,38 @@
+//
+//  GFTabbarController.swift
+//  GithubFollowers
+//
+//  Created by Christian Skorobogatow on 27/5/21.
+//
+
+import UIKit
+
+class GFTabbarController: UITabBarController {
+    
+
+    override func viewDidLoad() {
+        UITabBar.appearance().tintColor = .systemGreen
+        viewControllers                 = [createSearchNC(), createFavoritesNC()]
+    }
+    
+    
+    func createSearchNC() -> UINavigationController {
+        
+        let searchVC = SearchVC()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        
+        return UINavigationController(rootViewController: searchVC)
+        
+    }
+    
+    
+    func createFavoritesNC() -> UINavigationController {
+        
+        let favoritesVC = FavoriteListVC()
+        favoritesVC.title = "Favorites"
+        favoritesVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        
+        return UINavigationController(rootViewController: favoritesVC)
+    }
+}
