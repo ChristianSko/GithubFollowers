46572996aed6f9c87fde371f15a92090acac8c33
Christian Skorobogatow <christiansko@icloud.com>
2021-06-05 19:33:58 +0200

    refactored downloadAvatarImage

--- GithubFollowers/Custom Views/ViewControllers/GFUserInfoVC.swift
+++ GithubFollowers/Custom Views/ViewControllers/GFUserInfoVC.swift
@@ -36,7 +36,7 @@ class GFUserInfoVC: UIViewController {
     
     
     func configureUIElements() {
-        avatarImageView.downloadImage(from: user.avatarUrl)
+        downloadAvatarImage()
         userNameLabel.text          = user.login
         nameLabel.text              = user.name ?? ""
         locationLabel.text          = user.location ?? "No Location"
@@ -45,6 +45,16 @@ class GFUserInfoVC: UIViewController {
         
         locationImageView.image     = UIImage(systemName: SFSymbols.location)
         locationImageView.tintColor = .secondaryLabel
+    }
+    
+    func downloadAvatarImage() {
+        NetworkManager.shared.downloadImage(from: user.avatarUrl){ [weak self] image in
+            guard let self = self else { return }
+            DispatchQueue.main.async {
+                self.avatarImageView.image = image
+            }
+        }
+        
     }
     
     func addSubViews() {
