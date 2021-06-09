2e0491c2a79a019b6e082d87810bfe69c339c835
Christian Skorobogatow <christiansko@icloud.com>
2021-06-09 23:22:38 +0200

    feature: added tableView ext ReloadDataOnMainThread func for future on other projects

--- GithubFollowers/Extensions/UITableView+Ext.swift
+++ GithubFollowers/Extensions/UITableView+Ext.swift
@@ -10,6 +10,12 @@ import UIKit
 
 extension UITableView {
     
+    func reloadDataOnMainThread() {
+        DispatchQueue.main.async {
+            self.reloadData()
+        }
+    }
+    
     func removeExcessCells() {
         tableFooterView = UIView(frame: .zero)
     }
